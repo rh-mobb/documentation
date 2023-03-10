c3043e58f357d890d17d83d0596f22dc72cd3b9c
Kumudu Herath <kumuduh@gmail.com>
2023-03-10 08:52:07 -0800

    Removed old policies (#284)
    Removed cloud_credential_operator, image_registry_installer_cloud_credentials, ingress_operator_cloud_credentials, csi_drivers_ebs_cloud_credentials and cloud_network_config_controller_cloud_credentials policies.

--- content/docs/rosa/verify-permissions/verify-permissions.sh
+++ content/docs/rosa/verify-permissions/verify-permissions.sh
@@ -19,12 +19,6 @@ INSTALLER_POLICY=$(cat sts_installer_permission_policy.json | jq )
 CONTROL_PLANE_POLICY=$(cat sts_instance_controlplane_permission_policy.json | jq)
 WORKER_POLICY=$(cat sts_instance_worker_permission_policy.json | jq)
 SUPPORT_POLICY=$(cat sts_support_permission_policy.json | jq)
-CCO_POLICY=$(cat openshift_cloud_credential_operator_cloud_credential_operator_iam_ro_creds_policy.json | jq)
-REGISTRY_POLICY=$(cat openshift_image_registry_installer_cloud_credentials_policy.json | jq)
-INGRESS_POLICY=$(cat openshift_ingress_operator_cloud_credentials_policy.json | jq)
-CSI_POLICY=$(cat openshift_cluster_csi_drivers_ebs_cloud_credentials_policy.json | jq)
-NETWORK_POLICY=$(cat openshift_cloud_network_config_controller_cloud_credentials_policy.json | jq)
-MACHINE_POLICY=$(cat openshift_machine_api_aws_cloud_credentials_policy.json | jq)
 
 simulatePolicy () {
     outputFile="${2}.results"
@@ -36,8 +30,3 @@ simulatePolicy "$INSTALLER_POLICY" "sts_installer_permission_policy.json"
 simulatePolicy "$CONTROL_PLANE_POLICY" "sts_instance_controlplane_permission_policy.json"
 simulatePolicy "$WORKER_POLICY" "sts_instance_worker_permission_policy.json"
 simulatePolicy "$SUPPORT_POLICY" "sts_support_permission_policy.json"
-simulatePolicy "$CCO_POLICY" "openshift_cloud_credential_operator_cloud_credential_operator_iam_ro_creds_policy.json"
-simulatePolicy "$REGISTRY_POLICY" "openshift_image_registry_installer_cloud_credentials_policy.json"
-simulatePolicy "$INGRESS_POLICY" "openshift_ingress_operator_cloud_credentials_policy.json"
-simulatePolicy "$CSI_POLICY" "openshift_cluster_csi_drivers_ebs_cloud_credentials_policy.json"
-simulatePolicy "$NETWORK_POLICY" "openshift_cloud_network_config_controller_cloud_credentials_policy.json"
